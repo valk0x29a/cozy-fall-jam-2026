63356097defdcdd0c9d7255c45b78a02f86e2d8f
OpTiFall <62944006+OpTiFall@users.noreply.github.com>
2026-09-26 22:51:13 +0200

    hover ui + sprite test

--- scripts/valk/gather_seed_on_interact.gd
+++ scripts/valk/gather_seed_on_interact.gd
@@ -9,3 +9,7 @@ func player_interact() -> void:
     if(deactivate_after_use && used): return;
     GameManager.acquire_seed();
     used = true;
+
+func get_ui_text(): 
+    if(deactivate_after_use && used): return "";
+    return "Press 'E' to pick up a seed!!!";
